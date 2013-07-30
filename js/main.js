7789bbe056d52fe3014a6590f8b8dc4c7f80bfe1
Martin H. Normark <mhn@milkshakesoftware.com>
2013-07-31 01:33:21 +0200

    New structure, about section test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+(function() {
 
+	$(document.body).on("click", "a[href=#about]", function (event) {
+		event.preventDefault();
+
+		$("div.turn-anim-target").addClass("in");
+	});
+
+	$(document.body).on("click", "a[href=#close]", function (event) {
+		event.preventDefault();
+
+		$("div.turn-anim-target").removeClass("in");
+	});
+
+})();
